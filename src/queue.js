fc59384a6ac1b0524f4abdc4a0aa6deb8081e384
Alex Sidorov <promoabys@gmail.com>
2019-10-19 23:31:48 +0300

    inital: Partial solve of task

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,31 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
+		if(!maxSize) maxSize = 30;
+		this.maxSize = maxSize;	
+		this.heap = new MaxHeap();
 
+		
 	}
 
 	push(data, priority) {
-
+		// console.log(data, priority, )
+		if(data >= this.maxSize)  throw new Error("Queue has max size");
+		this.heap.push(data, priority);
+		//console.log(data, priority, this.heap);
 	}
 
-	shift() {
-
+	shift() {			
+        let lastValue = this.heap.pop();
+		return lastValue;
 	}
 
 	size() {
-
+	
 	}
 
 	isEmpty() {
-		
+	
 	}
 }
 
